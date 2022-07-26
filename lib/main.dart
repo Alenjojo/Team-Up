310db4bca3d5c10d4d5b33a46adf53f1e2501a8a
Alen Jojo <51394913+Alenjojo@users.noreply.github.com>
2022-07-26 17:25:50 +0530

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+
+import 'package:flutter/material.dart';
+void main() => runApp(Home());
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text("Team Up"),
+        ),
+      ),
+    );
+  }
+}
